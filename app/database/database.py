b7e117e6de8108d184d18a11305c307f8c6daf04
Jens Gäbeler <gaebeler@hdm-stuttgart.de>
2020-03-08 16:41:44 +0100

    back to ec2 with sqlite

--- app/database/database.py
+++ app/database/database.py
@@ -4,18 +4,32 @@ from sqlalchemy.orm import sessionmaker
 
 from app.common import credentials as cred
 
+from configparser import ConfigParser
+import platform
+
+
+config = ConfigParser()
+
+if platform.system() == "Linux":
+    config.read("/etc/credentials.ini")
+elif platform.system() == "Windows":
+    config.read("C:\\Dev\\credentials.ini")
+
+user = config.get("postgres", "user")
+pw = config.get("postgres", "password")
+uri = config.get("postgres", "endpoint")
+
+
 # SQLite
-# SQLALCHEMY_DATABASE_URL = "sqlite:///./sqlite.db"
-# engine = create_engine(
-#     SQLALCHEMY_DATABASE_URL, echo=True, connect_args={"check_same_thread": False}
-# )
+SQLALCHEMY_DATABASE_URL = "sqlite:///./sqlite.db"
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, echo=True, connect_args={"check_same_thread": False}
+)
 
 
 # PostgreSQL
-SQLALCHEMY_DATABASE_URL = "postgresql://{}:{}@{}".format(
-    cred.aws_rds["user"], cred.aws_rds["pw"], cred.aws_rds["uri"]
-)
-engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True, pool_pre_ping=True)
+# SQLALCHEMY_DATABASE_URL = "postgresql://{}:{}@{}".format(user, pw, uri)
+# engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True, pool_pre_ping=True)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 

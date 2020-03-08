37eb74a46e4b85d7d6efb37bcf7ff676b750bc31
Jens Gäbeler <gaebeler@hdm-stuttgart.de>
2020-03-08 16:58:24 +0100

    back to ec2 with sqlite

--- app/database/database.py
+++ app/database/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-from app.common import credentials as cred
+# from app.common import credentials as cred
 
 # from configparser import ConfigParser
 # import platform

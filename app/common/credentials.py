ab9f979ee660a411821396d2cc889a6313af7991
Jens Gäbeler <gaebeler@hdm-stuttgart.de>
2020-03-13 15:37:16 +0100

    postgres credentials connection

---
+++ app/common/credentials.py
@@ -0,0 +1,7 @@
+import os
+
+postgres_user = os.environ['POSTGRES_ENV_USER']
+postgres_pw = os.environ['POSTGRES_ENV_PW']
+postgres_uri = os.environ['POSTGRES_ENV_URI']
+
+
--- app/database/database.py
+++ app/database/database.py
@@ -2,34 +2,20 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# from app.common import credentials as cred
-
-# from configparser import ConfigParser
-# import platform
-#
-#
-# config = ConfigParser()
-#
-# if platform.system() == "Linux":
-#     config.read("/etc/credentials.ini")
-# elif platform.system() == "Windows":
-#     config.read("C:\\Dev\\credentials.ini")
-#
-# user = config.get("postgres", "user")
-# pw = config.get("postgres", "password")
-# uri = config.get("postgres", "endpoint")
-
+from app.common import credentials as cred
 
 # SQLite
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sqlite.db"
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, echo=True, connect_args={"check_same_thread": False}
-)
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./sqlite.db"
+# engine = create_engine(
+#     SQLALCHEMY_DATABASE_URL, echo=True, connect_args={"check_same_thread": False}
+# )
 
 
 # PostgreSQL
-# SQLALCHEMY_DATABASE_URL = "postgresql://{}:{}@{}".format(user, pw, uri)
-# engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True, pool_pre_ping=True)
+SQLALCHEMY_DATABASE_URL = "postgresql://{}:{}@{}".format(
+    cred.postgres_user, cred.postgres_pw, cred.postgres_uri
+)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True, pool_pre_ping=True)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 

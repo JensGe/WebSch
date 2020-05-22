885588d0295f76ec2c90064861dddf690dff79d7
Jens Gäbeler <gaebeler@hdm-stuttgart.de>
2020-05-23 01:50:42 +0200

    fix

--- app/database/pyd_models.py
+++ app/database/pyd_models.py
@@ -99,7 +99,6 @@ class GenerateRequest(BasisModel):
     max_url_amount: int = c.max_url
     visited_ratio: float = c.visited_ratio
     connection_amount: int = c.connections
-    fixed_crawl_delay: int = None
 
 
 class StatsResponse(BasisModel):

c97098582ca98bd1a63127be24ee022e721f0478
Jens Gäbeler <gaebeler@hdm-stuttgart.de>
2020-06-27 11:18:53 +0200

    fetcher settings default values

--- app/database/pyd_models.py
+++ app/database/pyd_models.py
@@ -136,23 +136,23 @@ class RandomUrls(BasisModel):
 
 class FetcherSettings(BasisModel):
 
-    logging_mode: int = None                        # 10: DEBUG, 20: INFO
-    crawling_speed_factor: float = None
-    default_crawl_delay: int = None
-    parallel_process: int = None
-    parallel_fetcher: int = None
+    logging_mode: int = 20
+    crawling_speed_factor: float = 10
+    default_crawl_delay: int = 5
+    parallel_process: int = 12
+    parallel_fetcher: int = 1
 
-    iterations: int = None
-    fqdn_amount: int = None
-    url_amount: int = None                          # 0: unlimited
+    iterations: int = 1
+    fqdn_amount: int = 10
+    url_amount: int = 0
 
-    short_term_prio_mode: enum.SHORTPRIO = None
-    long_term_prio_mode: enum.LONGPRIO = None
-    long_term_part_mode: enum.LONGPART = None
+    short_term_prio_mode: enum.SHORTPRIO = enum.SHORTPRIO.random
+    long_term_prio_mode: enum.LONGPRIO = enum.LONGPRIO.random
+    long_term_part_mode: enum.LONGPART = enum.LONGPART.none
 
-    min_links_per_page: int = None                  # Check Literature
-    max_links_per_page: int = None
-    lpp_distribution_type: enum.PAGELINKDISTR = None
+    min_links_per_page: int = 1
+    max_links_per_page: int = 1
+    lpp_distribution_type: enum.PAGELINKDISTR = enum.PAGELINKDISTR.discrete
 
-    internal_vs_external_threshold: float = None    # Check Literature
-    new_vs_existing_threshold: float = None
+    internal_vs_external_threshold: float = 1.0
+    new_vs_existing_threshold: float = 1.0
